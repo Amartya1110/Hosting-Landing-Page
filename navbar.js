644365a90b38170e002560eaab1ea0ae276482d3
Amartya Khan <amartyakhan999@gmail.com>
2022-08-17 14:13:58 +0530

    fixed hamburger menu issues

--- navbar.js
+++ navbar.js
@@ -1,7 +1,7 @@
 const hamMenu = document.getElementById("ham-menu");
 const hamBars = document.querySelectorAll(".menu-bar");
 const navMenu = document.querySelector(".nav-menu");
-console.log(hamBars);
+
 
 hamMenu.addEventListener("click", () => {
     navMenu.classList.toggle("toggle-bg");
@@ -10,3 +10,20 @@ hamMenu.addEventListener("click", () => {
         bar.classList.toggle("toggle-bar-bg");
     }
 })
+
+// if the viewport width is 920px or more then remove the above classes
+function removeClass() {
+    if(window.innerWidth >= 920) {
+        navMenu.classList.remove("toggle-bg");
+        hamMenu.classList.remove("toggle-ham");
+        for(let bar of hamBars) {
+            bar.classList.remove("toggle-bar-bg");
+        }
+    }
+}
+
+// Trigger the resize event
+window.addEventListener("resize", () => {
+    removeClass();
+})
+
